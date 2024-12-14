d7506a8629b8e01d79b9a989e0ba5d5e74363626
Pawan Kumar <pawankum0010@gmail.com>
2024-12-14 14:59:44 +0530

    add

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -5,12 +5,20 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Models\Category;
+
 
 
 class CategoryController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
+       $categories = Category::latest();
 
+       if(!empty($request->get('keyword'))){
+           $categories = $categories->where('name','like','%'.$request->get('keyword').'%');
+       }
+       $categories = $categories->paginate(10);
+       return view('admin.category.list', compact('categories'));
     }
 
     public function create() {
@@ -24,7 +32,17 @@ class CategoryController extends Controller
         ]);
 
         if ($validator->passes()) {
+            $category = new Category();
+            $category->name = $request->name;
+            $category->slug = $request->slug;
+            $category->status = $request->status;
+            $category->save();
 
+            $request->session()->flash('success','Category created successfully');
+            return response()->json([
+                'status' => true,
+                'message' => 'Category created successfully.',
+            ]);
         } else {
             return response()->json([
                 'status' => false,
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\http\Controllers\admin\AdminLoginController;
 use App\http\Controllers\admin\HomeController;
 use App\http\Controllers\admin\CategoryController;
+use Illuminate\http\Request;
 
 
 
@@ -35,9 +36,21 @@ Route::group(['prefix' => 'admin'], function () {
         Route::get('/logout', [HomeController::class, 'logout'])->name('admin.logout');
 
         // Category Route
+        Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
         Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
-        Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store'); 
+        Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
 
+
+        Route::get('/getSlug',function(Request $reques){
+            $slug = '';
+            if (!empty($reques->title)) {
+               $slug = Str::slug($reques->title);
+            }
+            return response()->json([
+                'status' => true,
+                'slug'   => $slug
+            ]);
+        })->name('getSlug');
     });
 });
 
